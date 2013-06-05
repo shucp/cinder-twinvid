bd08912dddcb4226c332ceeb0730a1202ea9eb4e
shucp <shu@chocopi.com>
2013-06-05 15:30:55 +0900

    key event video change.

--- src/TwinvidApp.cpp
+++ src/TwinvidApp.cpp
@@ -1,5 +1,6 @@
 #include "cinder/app/AppNative.h"
 #include "cinder/gl/gl.h"
+#include "cinder/qtime/QuickTime.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -7,28 +8,81 @@ using namespace std;
 
 class TwinvidApp : public AppNative {
   public:
+	void prepareSettings(Settings *settings);
 	void setup();
-	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+
+	// event
+	void keyDown(KeyEvent event);
+
+	// original function
+	void openVideo(qtime::MovieGlRef &mov);
+
+	// member
+	qtime::MovieGlRef playingMov, movA, movB;
+	char lastPressedKey;
+	gl::Texture frame;
 };
+
+void TwinvidApp::prepareSettings(Settings *settings)
+{
+	settings->setWindowSize(300, 300);
+	settings->setFrameRate(30.0f);
+}
 
 void TwinvidApp::setup()
 {
-}
+	openVideo(movA);
+	openVideo(movB);
 
-void TwinvidApp::mouseDown( MouseEvent event )
-{
+	gl::enableAlphaBlending();
+	gl::clear( Color( 0, 0, 0 ) ); 
 }
 
 void TwinvidApp::update()
 {
+	if (lastPressedKey) {
+		playingMov = movB;
+		lastPressedKey = NULL;
+	} else {
+		playingMov = movA;
+	}
+
+	if (playingMov) {
+		frame = playingMov->getTexture();
+	}
 }
 
 void TwinvidApp::draw()
 {
-	// clear out the window with black
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::draw(frame, getWindowBounds());
+}
+
+void TwinvidApp::keyDown(KeyEvent event)
+{
+	switch (event.getChar()) {
+		case KeyEvent::KEY_ESCAPE:
+			exit(0);
+			break;
+
+		case KeyEvent::KEY_RETURN:
+			setFullScreen(!isFullScreen());
+			break;
+
+		default:
+			lastPressedKey = event.getChar();
+	}
+}
+
+void TwinvidApp::openVideo(qtime::MovieGlRef &mov)
+{
+	string p = getOpenFilePath().string();
+	if (!p.empty()) {
+		mov = qtime::MovieGl::create(p);
+		mov->setLoop();
+		mov->play();
+	}
 }
 
 CINDER_APP_NATIVE( TwinvidApp, RendererGl )
